4f6e9e54bcbb946bcaa6808c1eb0413523813fdf
yushanhsieh <yushan.hsieh@gmail.com>
2014-11-25 11:45:22 +0800

    Create NewClass.php

---
+++ builds/NewClass.php
@@ -0,0 +1,13 @@
+<?php
+
+Class NewClass
+{
+        public function addNums($a,$b){
+
+                return $a + $b;
+        }
+
+        Public function aubtractNums($a,$b){
+                return $a - $b;
+        }
+}
