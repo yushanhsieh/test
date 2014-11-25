2adfda10cf28b17a65ff245950e3dd356911b665
yushanhsieh <yushan.hsieh@gmail.com>
2014-11-25 02:58:50 +0000

    add

---
+++ NewClassTest.php
@@ -0,0 +1,22 @@
+<?php
+//require_once 'PHPUnit/Autoload.php';
+require_once (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR.'application'.DIRECTORY_SEPARATOR . 'NewClass.php');
+
+Class NewClassTest extends PHPUnit_Framework_TestCase
+{
+	private $mobjNewClass;
+	
+	public function setup(){
+		$this->mobjNewClass = new NewClass();
+	}
+	
+	public function testAddNums(){
+		$linkResult = $this->mobjNewClass->addNums(1, 2);
+		
+		$this->assertEquals(3,$linkResult);
+	}
+	public function testSubtractNums(){
+		$linkResult = $this->mobjNewClass->aubtractNums(5, 1);
+		$this->assertEquals(4, $linkResult);
+	}
+}
