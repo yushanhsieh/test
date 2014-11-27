5a0790f6e5ee19d14700391b87ceb04af0b145a8
yushanhsieh <yushan.hsieh@gmail.com>
2014-11-27 11:28:35 +0800

    Create Test.php

---
+++ Test.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'PHPUnit/Autoload.php';
+require_once ('api/v2/include/DbHandler.php');
+
+Class NewClassTest extends PHPUnit_Framework_TestCase
+{
+	private $mobjNewClass;
+	
+	public function setup(){
+		$this->mobjNewClass = new DbHandler();
+	}
+	
+	public function testgetUserById(){
+		$linkResult = $this->mobjNewClass->getUserById(1);
+	}
+	
+}
